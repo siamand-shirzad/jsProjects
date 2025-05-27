dab15c3d9c338024ef944689133e1bc2fce8280b
siamand <siamand1381@gmail.com>
2025-05-27 18:24:27 +0330

    add and show list of costumers

--- assets/javascript/personalForm.js
+++ assets/javascript/personalForm.js
@@ -3,11 +3,58 @@ const nameInput = document.getElementById("name");
 const familyInput = document.getElementById("family");
 const emailInput = document.getElementById("email");
 const jobInput = document.getElementById("job");
+const phoneInput = document.getElementById("phone");
 const genderSelect = document.getElementById("gender");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
 
-function submit(params) {
-    console.log(124);
+let people = [];
+//افزودن کاربر به لیست
+form.addEventListener("submit", function(e){
+    e.preventDefault()
     
-    console.log(nameInput);
+    const newPerson = {
+        name: nameInput.value.trim(),
+        lastName : familyInput.value.trim(),
+        email : emailInput.value.trim(),
+        job : jobInput.value.trim(),
+        phone : phoneInput.value.trim(),
+        gender : genderSelect.value
+    }
+    people.push(newPerson);
+    form.reset();
     
-}
+})
+//نمایش همه کاربران
+document.getElementById("showModal").addEventListener("click", ()=>{
+    if (people.length === 0) {
+        modal.innerHTML = `<h3>هنوز کاربری ثبت نشده است</h3>`;
+    }else{
+        modal.innerHTML = `<h3>لیست کاربران:</h3>`;
+        const list = document.createElement("ul");
+        people.forEach((person ,index)=>{
+            const {name, lastName, email, job, phone, gender} = person
+
+            const li = document.createElement("li")
+            li.innerText = `${index+1}. ${name} ${lastName}
+            ایمیل:${email}
+            شغل:${job || '---'}
+            تلفن:${phone || '---'}
+            جنسیت:${gender || '---'}`;
+            list.appendChild(li)
+        })
+        modal.appendChild(list);
+    }
+    overlay.style.visibility = 'visible'
+    overlay.style.opacity = '1'
+    modal.style.visibility = 'visible'
+    modal.style.opacity = '1'
+    //بستن مدال با کلیک روی پس زمینه
+    overlay.addEventListener("click", () =>{
+        overlay.style.visibility = 'hidden'
+        overlay.style.opacity = '0'
+        modal.style.visibility = 'hidden'
+        modal.style.opacity = '0'
+
+    })
+});
