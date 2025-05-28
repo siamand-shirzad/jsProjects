5ed82ebf15eb9d094ba78bbc03529b1d66fbe33a
siamand <siamand1381@gmail.com>
2025-05-28 16:59:03 +0330

    find by email personalForm

--- assets/javascript/personalForm.js
+++ assets/javascript/personalForm.js
@@ -8,6 +8,7 @@ const genderSelect = document.getElementById("gender");
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
 
+
 let people = [];
 //افزودن کاربر به لیست
 form.addEventListener("submit", function(e){
@@ -49,12 +50,34 @@ document.getElementById("showModal").addEventListener("click", ()=>{
     overlay.style.opacity = '1'
     modal.style.visibility = 'visible'
     modal.style.opacity = '1'
-    //بستن مدال با کلیک روی پس زمینه
-    overlay.addEventListener("click", () =>{
-        overlay.style.visibility = 'hidden'
-        overlay.style.opacity = '0'
-        modal.style.visibility = 'hidden'
-        modal.style.opacity = '0'
-
-    })
 });
+//بستن مدال با کلیک روی پس زمینه
+overlay.addEventListener("click", () =>{
+    overlay.style.visibility = 'hidden'
+    overlay.style.opacity = '0'
+    modal.style.visibility = 'hidden'
+    modal.style.opacity = '0'
+
+})
+//پیدا کردن با ایمیا
+document.getElementById("findByEmail").addEventListener("click", ()=>{
+    const emailToFind = prompt("ایمیل مورد نظر را وارد کنید").trim();
+    const person = people.find((p)=>{return p.email === emailToFind})
+
+    if (person) {
+        const {name, lastName, email, job, phone, gender} = person
+        modal.innerHTML =`<h3>کاربر یافت شد</h3>
+        <p>نام و نام خانوادگی:${name}</p>
+        <p>ایمیل:${email}</p>
+        <p>شغل:${job}</p>
+        <p>تلفن:${phone}</p>
+        <p>جنسیت:${gender}</p>`
+    }else{
+        modal.innerHTML = `<p>کاربری یافت نشد</p>`
+    }    
+    overlay.style.visibility = 'visible'
+    overlay.style.opacity = '1'
+    modal.style.visibility = 'visible'
+    modal.style.opacity = '1'
+
+})
